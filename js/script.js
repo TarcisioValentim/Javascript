b2e742a840d5c91dda272a80a2f1f5ccf0a01b7b
Thrasys <tarcisio.word@gmail.com>
2020-08-12 21:07:39 -0300

    up portfolio em js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//backend
+function toggleMenu() {
+    var menuToggle = document.querySelector('.menuToggle');
+    var navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
